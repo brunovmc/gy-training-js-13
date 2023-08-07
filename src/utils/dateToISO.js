7d7ba2d33c3bc3beb4e73021c73ac8b95fb00688
brunovmc <bruno.vidigal.coriolano@gmail.com>
2023-08-07 14:13:41 -0300

    refactor: remove utils folder

--- src/utils/dateToISO.js
+++
@@ -1,9 +0,0 @@
-function dateToISO(dateTimeString) {
-  const dateString = dateTimeString.split(' ')[0];
-  const dateParts = dateString.split('/');
-  return `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
-}
-
-module.exports = {
-  dateToISO
-};
